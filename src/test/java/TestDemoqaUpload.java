4fa1fb0eed3ddeadb38b2fba0f7d257ce0a91690
Deniz Albayrak <92841021+1SeaMy@users.noreply.github.com>
2024-08-24 01:07:59 +0300

    Update TestDemoqaUpload.java

--- src/test/java/TestDemoqaUpload.java
+++ src/test/java/TestDemoqaUpload.java
@@ -31,7 +31,7 @@ public class TestDemoqaUpload {
 
         WebElement uploadMes=driver.findElement(By.id("uploadedFilePath"));
         Assert.assertTrue(uploadMes.isDisplayed());
-        Assert.assertEquals(uploadMes.getText(),"C:\\fakepath\\1.jpg");
+        Assert.assertEquals(uploadMes.getText(),"C:\\fakepath\\111.jpg");
         System.out.println(uploadMes.getText());
         driver.close();
     }
